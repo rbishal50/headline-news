20044985668db48ded61457b54eba3e46c682b96
Bishal Rana <rbishal50@gmail.com>
2021-01-28 18:34:56 +0545

    Remove axios and add a request util function using fetch

--- src/redux/api/newsApi.ts
+++ src/redux/api/newsApi.ts
@@ -1,11 +1,13 @@
-import axios from 'axios'
+import { request } from '../../utils/request'
 
 export async function getHeadlines(): Promise<[]> {
   const url = `${process.env.REACT_APP_API_BASE_URL}/top-headlines?sources=techcrunch&apiKey=${process.env.REACT_APP_API_KEY}`
 
   try {
-    const { data } = await axios.get<any>(url)
-    return data.articles
+    const { articles } = await request(url, {
+      method: 'GET',
+    })
+    return articles
   } catch (err) {
     throw err
   }
